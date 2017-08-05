ce58f7e28532715b3758579ec4745fbef7e5dfca
Benson Maruchu <benmaruchu@gmail.com>
2017-08-05 13:43:54 +0300

    Add initial api documentation

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "open311-api",
+  "version": "0.1.0",
+  "description": "open311 API",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-08-05T10:39:18.283Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
